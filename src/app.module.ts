93e2065311be46badb5e4705d7fff6da70ad01ef
prcordova <pedroricardocordova@yahoo.com.br>
2024-01-29 16:41:34 -0300

    fix: dotenv installed to dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from "@nestjs/common";
 import { UsersModule } from "./users/users.module";
 import { MongooseModule } from "@nestjs/mongoose";
+import { config } from "dotenv";
+
+config();
 
 @Module({
   imports: [MongooseModule.forRoot(process.env.MONGO_URL), UsersModule],
